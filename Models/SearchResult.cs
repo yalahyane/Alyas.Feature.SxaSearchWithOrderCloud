15f4bb027de593689a148386a573d205085fe20d
Yassine Alahyane <yassine.alahyane@alyassolutions.com>
2023-08-22 15:39:58 -0400

    Simplify Search Results

--- Models/SearchResult.cs
+++ Models/SearchResult.cs
@@ -11,24 +11,12 @@ namespace Alyas.Feature.SxaSearchWithOrderCloud.Models
 
         public SearchResult(ContentPage searchResult)
         {
-            if (searchResult.Fields["_datasource"].ToString().Equals("OrderCloud", StringComparison.OrdinalIgnoreCase))
-            {
-                this.IsProduct = true;
-                this.Id = searchResult.Fields["_name"].ToString();
-                this.Language = searchResult.Fields["_language"].ToString();
-                this.Name = searchResult.Fields["_displayname"].ToString();
-                this.Url = searchResult.Fields["_fullpath"].ToString();
-                this.Path = searchResult.Fields["_fullpath"].ToString();
-            }
-            else
-            {
-                this.IsProduct = false;
-                this.Id = searchResult.ItemId.ToString();
-                this.Language = searchResult.Language;
-                this.Path = searchResult.Path;
-                this.Name = searchResult.Name;
-                this.Url = searchResult.Url;
-            }
+            this.IsProduct = searchResult.Fields["_datasource"].ToString().Equals("OrderCloud", StringComparison.OrdinalIgnoreCase);
+            this.Id = searchResult.ItemId.ToString();
+            this.Language = searchResult.Language;
+            this.Path = searchResult.Path;
+            this.Name = searchResult.Name;
+            this.Url = searchResult.Url;
         }
     }
 }
